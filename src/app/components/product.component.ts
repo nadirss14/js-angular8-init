e10131853808c10bed5c077a6cdc1b994fdf207e
nadirss14 <nadirss14@gmail.com>
2019-12-20 16:49:17 -0600

    initial commit

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { IProduct } from "./products/product.model";
+@Component({
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"]
+})
+export class ProductComponent {
+  @Input() product: IProduct;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  addCart(title: string) {
+    console.log(`Se añadio el articulo ${title} al carrito`);
+    this.productClicked.emit(this.product.id);
+  }
+}
---
+++ src/app/components/products/product.model.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+  moneda: string;
+}
